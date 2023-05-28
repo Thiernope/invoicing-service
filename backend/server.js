6ef6c3d7ba954d4bc791e4fe593511f82b9e2eea
octan-6 <ntirandth@gmail.com>
2023-05-28 22:02:27 +0200

    adding logs with winston and morgan to track the events in the application

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 const config = dotenv.config();
 
 const App = express();
@@ -14,10 +15,17 @@ if(process.env.NODE_ENV === "development") {
     App.use(morgan("dev"));
 }
 
+App.use(morganMiddleware);
+
 App.get("/api/v1/test", (req, res)=> {
     res.json({message: "Welcome to my invoice app"});
 })
 
 const PORT = process.env.PORT || 4000;
 
-App.listen(PORT, () => console.log(`${chalk.green.bold("✓")} App running on port ${chalk.blue.bold(`${PORT}`)} and on ${chalk.yellow.bold(`${process.env.NODE_ENV}`)}`));
+App.listen(PORT, () => {
+console.log(`${chalk.green.bold("✓")} App running on port ${chalk.blue.bold(`${PORT}`)} and on ${chalk.yellow.bold(`${process.env.NODE_ENV}`)}`)
+systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+);
+});
